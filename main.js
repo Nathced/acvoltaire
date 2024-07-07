c263bc06cbcd282b31c9fae64d44310651c49475
Nathced <156846829+Nathced@users.noreply.github.com>
2024-07-07 16:35:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("header nav");
     const links = document.querySelectorAll("nav ul li a");
-    const sections = document.querySelectorAll(".content");
+
+    menuToggle.addEventListener("click", function() {
+        nav.classList.toggle("nav-active");
+    });
 
     links.forEach(link => {
         link.addEventListener("click", function(e) {
             e.preventDefault();
-            const targetSection = document.querySelector(`#${this.dataset.section}`);
-
-            sections.forEach(section => {
-                section.classList.remove("active");
-            });
-
-            targetSection.classList.add("active");
+            const section = document.querySelector(`#${this.dataset.section}`);
+            document.querySelectorAll(".content").forEach(content => content.classList.remove("active"));
+            section.classList.add("active");
+            nav.classList.remove("nav-active");
         });
     });
 });
